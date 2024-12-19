51abdc163850f889ef029b302c261d6bd2416d68
NotWorle <duchuy.notworle@gmail.com>
2024-12-19 15:42:17 +0700

    Load

--- GameHub/accounts/views.py
+++ GameHub/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-# Create your views here.
+from django.http import HttpResponse
+
 
 def login(request):
     return render(request, 'login.html')
