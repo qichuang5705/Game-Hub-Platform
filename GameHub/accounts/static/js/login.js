959ac7390bc6b2141425e885630b5aec3eeac8ba
NotWorle <duchuy.notworle@gmail.com>
2024-12-20 15:57:36 +0700

    web login, home, register

--- GameHub/accounts/static/js/login.js
+++ GameHub/accounts/static/js/login.js
@@ -1,12 +1,33 @@
-function ChangeBackA(){
-    document.getElementById("demo").innerHTML = "After"
-}
-function ChangeBackB(){
-    document.getElementById("demo").innerHTML = "Before"
+export const emailInput = document.getElementById('exampleInputEmail');
+export const passwordInput = document.getElementById('exampleInputPassword');
+export const submitButton = document.getElementById('submitButton');
+
+export const toggleSubmitButton = () => {
+    console.log('vaooooo')
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  // Enable the submit button only if both email and password are not empty
+  submitButton.disabled = !(email && password);
 }
 
+export const handleFormSubmit = async(event)=> {
+  event.preventDefault(); // Ngăn chặn hành động mặc định của form (reload trang)
 
-function changeBackground() {
-    let randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    document.body.style.backgroundColor = randomColor;
+  // Lấy dữ liệu từ các input
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  try {
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    // Tạm dừng 500ms trước khi chuyển trang
+    setTimeout(() => {
+      //window.location.href = '/pages/register.html';
+    }, 500);
+  } catch (error) {
+    console.error('Error:', error);
+    alert('An error occurred. Please try again.');
+  }
 }
--- GameHub/accounts/urls.py
+++ GameHub/accounts/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('login2', views.login, name="login"),
-    path('login2/register', views.register, name="register"),
+    path('loginas', views.login, name="login"),
+    path('loginas/register', views.register, name="register"),
 ]
