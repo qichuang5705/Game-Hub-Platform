2984f0ab76330ad71c23c3f4377d97f46755b996
Lê Đức Huy <duchuy.notworle@gmail.com>
2025-01-13 04:29:27 +0700

    Xong chức năng chỉnh sữa thông tin user

---
+++ in4user/account/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import *  
+from django import forms
+
+class FormUser(UserCreationForm):
+   class Meta:
+        model = User  # Sử dụng mô hình tùy chỉnh
+        fields = ('username', 'password1', 'password2')
+
+
+
+class APIin4(forms.ModelForm):
+   class Meta:
+        model = User 
+        fields = ('first_name', 'last_name', 'email')
---
+++ in4user/account/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.http.response import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .forms import *
+
+def home(request):
+    return render(request, 'home.html', {'user': request.user})
+
+
+def login(request):
+    return render(request, 'login.html')
+
+def changepass(request):
+    return render(request, 'password_change_for.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = FormUser(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = FormUser()
+    return render(request, "signup.html", {"form": form})
+
+def in4(request):
+    user = request.user
+    if request.method == "POST":
+        form = APIin4(request.POST,  instance=user)
+        if form.is_valid():
+            form.save()
+    else:
+        form = APIin4( instance=user)
+    return render(request, "in4user.html", {'form': form})
+
