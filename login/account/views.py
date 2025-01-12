5a6f9bbbbfeeab1c8a64d906c008477cca1a3677
Lê Đức Huy <duchuy.notworle@gmail.com>
2025-01-12 19:47:06 +0700

    Sữa lỗi đăng ký

--- login/account/views.py
+++ login/account/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -17,6 +17,7 @@ def signup(request):
         form = UserCreationForm(request.POST or None)
         if form.is_valid():
             form.save()
+            return redirect("login")
     else:
         form = UserCreationForm()
     return render(request, "signup.html", {"form": form})
