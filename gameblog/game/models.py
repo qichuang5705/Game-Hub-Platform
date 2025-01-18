f7b7a1b06c40e29996e4c3284d54aa5ae4b6acf3
Lê Đức Huy <duchuy.notworle@gmail.com>
2025-01-18 15:54:47 +0700

    Đăng tải game

--- gameblog/game/models.py
+++ gameblog/game/models.py
@@ -13,7 +13,16 @@ class Game(models.Model):
     datecreate = models.DateField(auto_now_add=True)
     reward = models.IntegerField(default=0)
     image = models.ImageField(upload_to='image/', null=True, blank=True)
-
+    file = models.FileField(upload_to='games', null=True, blank=True)
+    game_type = models.CharField(
+        max_length=50,
+        choices=[('html5', 'HTML5'), ('unity-webgl', 'Unity WebGL')],
+        default='html5'
+    )
+    def get_file_url(self):
+        if self.file:
+            return self.file.url  # URL truy cập file đã upload
+        return None
 class Comment(models.Model):
     games = models.ForeignKey(Game, on_delete=models.CASCADE)
     users = models.ForeignKey(User, on_delete=models.CASCADE)
