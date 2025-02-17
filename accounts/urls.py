4ff7021f98081977705d24471f5502989aee5711
pthang1001 <thangnp0470@ut.edu.vn>
2025-02-17 11:27:14 +0700

    update error

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,4 +16,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_done.html"), name="password_reset_complete"),
     # path('login_register/', views.login_register, name="login_register")
+    path('error/', views.error, name="erro"),
 ]
