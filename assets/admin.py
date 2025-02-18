25e113e6a7565c1e440c921569d710932101016e
DuYB4ng <quynhphung11@gmail.com>
2025-02-19 00:54:31 +0700

    Sua lai chuc nag dang hinh , them giao dien thanh toan

--- assets/admin.py
+++ assets/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import asset
+admin.site.register(asset)
 
-@admin.register(asset)
 class AssetAdmin(admin.ModelAdmin):
     list_display = ('price', 'type', 'description','quantifier','title','thumnail')  
