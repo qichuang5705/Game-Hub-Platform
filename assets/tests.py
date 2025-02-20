ec16e81fcaa5a9b031efa71424504c37b39a7a3e
Lê Đức Huy <duchuy.notworle@gmail.com>
2025-02-20 12:27:51 +0700

    Sủa testing

--- assets/tests.py
+++ assets/tests.py
@@ -5,31 +5,6 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.db.utils import IntegrityError
 from django.core.exceptions import ValidationError
 
-<<<<<<< HEAD
-
-from django.test import TestCase
-from .models import Asset
-from django.core.files.uploadedfile import SimpleUploadedFile
-
-class AssetModelTest(TestCase):
-    def setUp(self):
-        """Tạo dữ liệu mẫu cho kiểm thử"""
-        self.asset = Asset.objects.create(
-            name="Test Asset",
-            description="This is a test asset",
-            image=SimpleUploadedFile("test_image.jpg", b"file_content", content_type="image/jpeg"),
-        )
-
-    def test_asset_creation(self):
-        """Kiểm tra xem asset có được tạo thành công không"""
-        asset = Asset.objects.get(name="Test Asset")
-        self.assertEqual(asset.description, "This is a test asset")
-
-    def test_asset_str_method(self):
-        """Kiểm tra phương thức __str__ của model"""
-        asset = Asset.objects.get(name="Test Asset")
-        self.assertEqual(str(asset), "Test Asset")
-=======
 user = get_user_model()
 
 class AssetModelTest(TestCase):
@@ -109,7 +84,7 @@ class PurchaseModelTest(TestCase):
         with self.assertRaises(ValidationError):
             invalid_purchase=Purchase(
                 user=self.user,
-                asset=self.asset,payment_method='bitcoin'   
+                asset=self.asset,
+                payment_method='bitcoin'   
             )
             invalid_purchase.full_clean()
->>>>>>> lab
