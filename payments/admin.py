0f13a843367379f007e753e7f5efc5aab238ea99
DuYB4ng <quynhphung11@gmail.com>
2025-02-22 15:27:53 +0700

    Hoan THien Thanh Toan

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Wallet
+
+@admin.register(Wallet)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('user', 'balance')
