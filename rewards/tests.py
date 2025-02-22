314ad701f04d645986fa0cf83477301bcecc7536
Lê Đức Huy <duchuy.notworle@gmail.com>
2025-02-22 20:07:04 +0700

    testing mua phần thưởng

--- rewards/tests.py
+++ rewards/tests.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
-
+import time
 browser = webdriver.Chrome()
 
 browser.get("http://localhost:8000")  # Cập nhật URL nếu cần
@@ -45,5 +45,31 @@ doi_thuong_button = WebDriverWait(browser, 10).until(
 doi_thuong_button.click()
 print("Đã ấn nút Đổi thưởng!")
 
-WebDriverWait(browser, 10).until(EC.url_contains("/bag"))
-print("Đã vào trang Đổi thưởng!")
+
+
+openuser = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "open2")))
+openuser.click()
+print("open1 oke")
+
+
+shop_tab = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.ID, "shopbaga")))
+shop_tab.click()
+print("Đã chuyển sang trang Shop!")
+
+
+
+
+
+# Ấn vào nút "Mua"
+buy_button = WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "buy-button")))
+buy_button.click()
+print("Đã ấn nút Mua!")
+
+
+
+# Xử lý hộp thoại confirm
+WebDriverWait(browser, 3).until(EC.alert_is_present())
+alert = browser.switch_to.alert
+alert.accept()
+print("Đã chấp nhận xác nhận mua hàng!")
+
